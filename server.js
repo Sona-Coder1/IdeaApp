92655acf458eae47ae92deac01175b9210ce9df4
Sona Pathak <sonapathak@Sonas-MacBook-Air.local>
2023-09-21 17:31:48 +0530

    Adding the server config

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const serverConfig = require('./Configs/server.config')
 
 const app = express();
 
 
 
-app.listen(3333, ()=>{
-    console.log("server started");
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}` );
 })
