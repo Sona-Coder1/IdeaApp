bcbc32c614f5a3cd78319c6f83bc4925d769a9ca
Sona Pathak <sonapathak@Sonas-MacBook-Air.local>
2023-09-22 10:03:36 +0530

    connecting to mongoDB

---
+++ models/user.model.js
@@ -0,0 +1,45 @@
+/*
+    This will hold the schema for the user
+
+    It explains the different fields of the user and how it will be stored in the mongoDB
+*/
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    name : {
+        type : String,
+        required : true
+    },
+    userID : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        minLength : 10,
+        lowercase : true
+    },
+    userType : {
+        type : String,
+        required : true,
+        deafult : "CUSTOMER",
+        enum : ["CUSTOMER", "ADMIN"]
+    }
+
+
+},{timestamps : true})
+
+/**
+ * Define the collection name where it will be stored 
+ */
+
+module.exports = mongoose.model("user", userSchema); // defines the collectuon in which this schema will be used
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const serverConfig = require('./Configs/server.config')
+const mongoose = require('mongoose')
+const dbConfig = require('./Configs/db.config');
 
 const app = express();
+
+/**
+ * Logic to connect to mongoDB and create an admin user
+ * Need to have mongoDb up and running in your local machine
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to Database");
+});
+
+db.once("open", ()=>{
+    console.log("Database is connected");
+});
+
 
 
 
