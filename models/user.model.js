d538307a1c84e99e8140367cf2587a7af4e0ebd4
Sona Pathak <sonapathak@Sonas-MacBook-Air.local>
2023-09-22 21:37:22 +0530

    Data stored in database

--- models/user.model.js
+++ models/user.model.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true
     },
-    userID : {
+    userId : {
         type : String,
         required : true,
         unique : true
--- server.js
+++ server.js
@@ -1,28 +1,58 @@
-const express = require('express');
-const serverConfig = require('./Configs/server.config')
-const mongoose = require('mongoose')
+const express  = require('express');
+const serverConfig =require('./configs/server.config');
+const mongoose = require('mongoose');
 const dbConfig = require('./Configs/db.config');
+const userModel = require('./models/user.model');
+
 
 const app = express();
 
+
 /**
- * Logic to connect to mongoDB and create an admin user
- * Need to have mongoDb up and running in your local machine
+ * Logic to connect to MongoDB and create an ADMIN user
+ * Need to have the mongodb up and running in your local machine
  */
 mongoose.connect(dbConfig.DB_URL);
 const db = mongoose.connection ;
 
 db.on("error", ()=>{
-    console.log("Error while connecting to Database");
+    console.log("Error while connecting to DB");
 });
 
 db.once("open", ()=>{
-    console.log("Database is connected");
-});
+    console.log("DB is connected");
 
+    init();
+})
+
+async function  init(){
+    
+    /**
+     * Check if the admin user is already present
+     */
+    let admin = await userModel.findOne({
+        userId : "admin"
+    })
+
+    if(admin){
+        console.log("Admin user already present");
+        return;
+    }
+
+    admin = await userModel.create( {
+        name : "Sona Pathak",
+        userId : "admin",
+        email : "sonapathak49@gmail.com",
+        userType : "ADMIN",
+        password : "Welcome"
+    });
+    console.log(admin);
+
+    
+}
 
 
 
 app.listen(serverConfig.PORT, ()=>{
-    console.log(`server started on the port number ${serverConfig.PORT}` );
+    console.log(`server started on the port number ${serverConfig.PORT}`);
 })
